a569faaf46f0ead293717049bfc0107b905cbdff
LianBorda <>
2021-04-22 22:37:48 +0000

    Reto 28 ok (SM)

---
+++ py2_retos1/reto28.py
@@ -0,0 +1,12 @@
+
+print('     TABLAS DE MULTIPLICAR')
+
+def multiplicador(numero):
+  resultado= numero*e
+  return resultado
+
+for i in range(0,11):
+  print(f'\nTABLA DEL {i}')
+  for e in  range(0,13):
+    print(f'{i} x {e} = {multiplicador(i)}')
+  
