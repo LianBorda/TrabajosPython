3380c741afa2b58a2ad107c638806351be981a11
LianBorda <>
2021-04-22 19:52:28 +0000

    Reto 4 ok (SM)

---
+++ py2_retos1/reto4.py
@@ -0,0 +1,12 @@
+
+
+
+n_1 = int(input("Ingrese el primer numero entero ")) 
+n_2 = int(input("Ingrese el primer numero entero "))
+
+if n_1>n_2:
+  print(f'El primer número es mayor. ({n_1}>{n_2})')
+elif n_1<n_2:
+  print(f'El primer número es menor. ({n_1}<{n_2})')
+else:
+  print(f'Es el mismo número. ({n_1}={n_2})')
