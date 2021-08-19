d6289b387a18768577870324bd4029e0906fa33c
LianBorda <81332414+LianBorda@users.noreply.github.com>
2021-08-19 20:37:02 +0000

    Reto 13 OK SM

---
+++ py6_retos2 /reto13.py
@@ -0,0 +1,10 @@
+
+print("\n MEDIA DE DOS NUMEROS (Promedio)\n")
+
+Num_1= float(input("Ingrese el primer numero: ")) 
+Num_2= float(input("Ingrese el segundo numero: "))
+Promedio= (Num_1+Num_2)/2
+
+print('\n   SOLUCION\nPrimer número:',Num_1)
+print('Segundo número:',Num_2)
+print('Promedio:',Promedio)
