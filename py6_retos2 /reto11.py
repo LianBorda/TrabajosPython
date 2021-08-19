19926d945701171e0d029f4da2d7542a498e4452
LianBorda <81332414+LianBorda@users.noreply.github.com>
2021-08-19 20:23:48 +0000

    reto 11 Ok SM

---
+++ py6_retos2 /reto11.py
@@ -0,0 +1,9 @@
+print("\n UNIR LISTAS\n")
+
+Lista_1=list(range(0,11))
+Lista_2=list(range(11,21))
+Lista_resultado=Lista_1+Lista_2
+
+print("Lista 1: ",Lista_1,"\nLista 2: ",Lista_2,"\n\nLista total: ",Lista_resultado)
+
+
